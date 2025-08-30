f0c43d47962dcf03caee78db995e723c03a687c4
Darsh <darshdcoding@gmail.com>
2025-08-31 01:02:53 +0530

    initial commit with complete project

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("Radhe Radhe....")
+
+const firstName= document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const termCheck = document.getElementById("term-agree");
+const form = document.getElementById("main-form")
+let query;
+const queryError = document.getElementById("query-error")
+const generalType = document.getElementById("general-query-container");
+const supportType = document.getElementById("support-request-container");
+const radioInputs = document.querySelectorAll(".radio-options");
+
+// Function to handle radio button selection
+radioInputs.forEach((input) => {
+    input.addEventListener("change", () => {
+        if (input.value === "general") {
+            generalType.classList.add("active-check");
+            supportType.classList.remove("active-check");
+        } else if (input.value === "support") {
+            supportType.classList.add("active-check");
+            generalType.classList.remove("active-check");
+        }
+    });
+});
+
+//validating form
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const elementList = [firstName, lastName, email, message]
+    elementList.forEach(element =>{
+        const error= element.nextElementSibling;
+        if (!element.checkValidity()){
+            element.style.borderColor = "var(--primary-red)";
+            error.innerText = element.validationMessage;
+        }
+        else{
+            element.style.borderColor = "var(--neutral-gray-dark)";
+            error.innerText = "";
+        }
+    });
+    try{
+        query = document.querySelector('input[name="query-type"]:checked').value;
+        queryError.innerText = ""
+    }
+    catch (error){
+        queryError.innerText = "This field is required"
+    }
+    const termError= document.getElementById("term-error")
+    try{
+        document.querySelector('input[name="term-agree"]:checked').value;
+        termError.innerText = ""
+    }catch (error){
+        termError.innerText = "To submit this form, please consent to being contacted"
+    };
+})
